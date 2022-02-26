036948aea65f72e4ebafa4ec7a95aa7afc8e33cf
Adrian Girlea <girlea.adrian@isa.utm.md>
2022-02-26 12:59:57 +0200

    Change ASCII characters density

--- src/com/company/asciiconverter/BasicAsciiConverter.java
+++ src/com/company/asciiconverter/BasicAsciiConverter.java
@@ -3,9 +3,10 @@ package com.company.asciiconverter;
 import com.company.asciiconverter.abst.IAsciiConverter;
 
 public class BasicAsciiConverter implements IAsciiConverter {
-    private final static String ASCII_CHARACTERS = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'.  ";
-    private final static String DENSITY = "Ñ@#W$9876543210?!abc;:+=-,._   ";
+    private final static String ASCII_CHARACTERS = "$@B%8&WM#ZO0QLCJUYXzcvunxrjft/\\)1}]|?-_+~<>i!lI;:,\"^`'.  ";
+//    private final static String DENSITY = "Ñ@#W$9876543210?!abc;:+=-,._   ";
 
+    @Override
     public char getChar(int rgbValue) {
         int asciiLength = ASCII_CHARACTERS.length() - 1;
         int charIndex = (int) (Math.ceil(((asciiLength) * rgbValue) / (float) 255));
