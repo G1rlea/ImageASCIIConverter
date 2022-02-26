63577ebd15f520c2eafab22ce297e75ae482e410
Adrian Girlea <girlea.adrian@isa.utm.md>
2022-02-26 12:53:16 +0200

    make the convert method static

--- src/com/company/ImageAsciiConverter.java
+++ src/com/company/ImageAsciiConverter.java
@@ -8,14 +8,8 @@ import com.company.rgbcalculator.EyeSensitiveRgbValueCalculator;
 //God object
 public class ImageAsciiConverter {
 
-    private final String filePath;
-
-    public ImageAsciiConverter(String filePath){
-        this.filePath = filePath;
-    }
-
-    public void convert(){
-        ImageReader imageReader = new ImageReader( new EyeSensitiveRgbValueCalculator(), filePath);
+    public static void convert(String filePath) {
+        ImageReader imageReader = new ImageReader(new EyeSensitiveRgbValueCalculator(), filePath);
         var pixelRgbValues = imageReader.getRgbValueArray();
 
         AsciiFileWriter fileWriter = new AsciiFileWriter(new BasicAsciiConverter(), filePath);
